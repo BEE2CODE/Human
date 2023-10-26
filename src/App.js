b58bfdb66acb7eaa1e856a696efa9a54498b81ce
Oluwabunmi Onifade <Oluwabunmi.Onifade@fcmb.com>
2023-10-26 16:38:09 +0100

    Human counter application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increment(){
+    setCount (prevCount => prevCount + 1)
+  }
+  function decrement(){
+    setCount (count - 1)
+    if(count <=1){
+      setCount (0)
+    }
+  }
+  function reset(){
+    setCount(0)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wrap">
+        <h1 className="count-el">{count}</h1>
+        <button onClick={increment} className="increment">INCREMENT</button>
+        <button onClick={decrement}  className="decrement">DECREMENT</button>
+        <button onClick={reset}  className="reset">RESET</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Error.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// export default function Error404() {
+//   return (
+//     <div className="error-page">
+//       <h1>404 Error: Page Not Found</h1>
+//       <p>The page you are looking for does not exist.</p>
+//     </div>
+//   );
+// }
